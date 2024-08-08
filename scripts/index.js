d5951a97ac874a448b63878f0feb3bdb4b5a4a7b
Daria Leskina <dleskinaleskina@yandex.ru>
2024-08-08 14:46:20 +0300

    коммит1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
+// Получаем ссылку на шаблон и контейнер, в который положим карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardContainer = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+// Функция для отрисовки карточки
+function createCard(cardData, onDelete) {
+  // клонируем содержимое шаблона
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
 
-// @todo: Функция создания карточки
+  // наполняем содержимым
+  const cardImage = (cardElement.querySelector(".card__image").src =
+    cardData.link);
+  const cardTitle = (cardElement.querySelector(".card__title").textContent =
+    cardData.name);
+  cardImage.alt = cardData.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  // добавляем обработчик клика
+  deleteButton.addEventListener("click", () => {
+    onDelete(cardElement);
+  });
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Функция для удаления карточки
+function deleteCard(cardElement) {
+    cardContainer.removeChild(cardElement);
+  }
+
+// используем массив с карточками
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, deleteCard);
+  cardContainer.appendChild(card);
+});
